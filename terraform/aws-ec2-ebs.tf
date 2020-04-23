ee8ba4d4a7afe09a5ab15a540e87254b8c77e063
dineshvelmuruga <dinesh.velmuruga@tigeranalytics.com>
2020-04-23 09:49:06 +0000

    added readme

--- terraform/aws-ec2-ebs.tf
+++ terraform/aws-ec2-ebs.tf
@@ -5,8 +5,7 @@ provider "aws" {
 
 resource "aws_key_pair" "tiger_tf_kp" {
   key_name   = "tiger-mle-tf-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAmnD30IFt/NEv0f33PZ+xklkuaE0ZWt8uEyeAkJ+tOYbpkDBPNMsBp5FwGzsKQvmZLAn4vGrrawVkWyeugT35R5KVrCbmlenhicehA67MZtucgre80Wj4070Z2MlI+tljD57HRabJNuxoFUcL/qbGVMldy1go4VQASmVXB9M4M0Op5MLij2I7LCFdCFrBC71c7+0k/Y0bCwh894gpL6t9P7Qg7cXw/XcANGxPD/Du4i6WZPdMfQ65Cxgp8pS1xeAYfqBH0HPTfTtIMnafFsSS0oJpx1C0Yb8xsISzDfEAUVkzq8qp1nO6MePFCEbjpGG59V3O6HbFMrMJqqXY4T3Viw== rsa-key-20200423"
-}
+  public_key = "<ssh key>"}
 
 resource "aws_instance" "tiger_mle_tf_ec2" {
   ami           = "ami-0323c3dd2da7fb37d"
