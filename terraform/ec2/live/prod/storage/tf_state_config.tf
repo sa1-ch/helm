dc3d4bec84fd97fcaa280359b1129f73e143860c
dineshvelmuruga <dinesh.velmuruga@tigeranalytics.com>
2020-06-14 12:44:28 +0530

    Update tf_state_config.tf

--- terraform/ec2/live/prod/storage/tf_state_config.tf
+++ terraform/ec2/live/prod/storage/tf_state_config.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
+    # Cant use variables in terraform config since hardcoded
     bucket         = "mle-terraform-state"
-    key            = "staging/ebs/terraform.tfstate"
+    key            = "prod/ebs/terraform.tfstate"
     region         = "us-east-1"
 
     # Replace this with your DynamoDB table name!
