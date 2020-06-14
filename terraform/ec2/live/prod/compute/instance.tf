c07ac0458bbec1c6daf88084cbfdda3b2228b3c8
dineshvelmuruga <dinesh.velmuruga@tigeranalytics.com>
2020-06-14 12:41:08 +0530

    Update instance.tf

--- terraform/ec2/live/prod/compute/instance.tf
+++ terraform/ec2/live/prod/compute/instance.tf
@@ -2,7 +2,7 @@ module "dev_compute" {
   source = "../../modules/compute"
 
   state_bucket = "mle-terraform-state"
-  vpc_state_key = "staging/vpc/terraform.tfstate"
+  vpc_state_key = "prod/vpc/terraform.tfstate"
   aws_region = "us-east-1"
   instance_ami = "ami-0323c3dd2da7fb37d"
   ec2_instance_type = "t2.micro"
